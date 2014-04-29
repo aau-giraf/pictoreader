9fc268790508a4454b77ad6422a4a0b082096418
Kaysen <ckaysen@gmail.com>
2014-04-29 10:15:01 +0200

    GUI halløj

--- src/dk/aau/cs/giraf/parrot/OptionFragment.java
+++ src/dk/aau/cs/giraf/parrot/OptionFragment.java
@@ -6,8 +6,6 @@ import yuku.ambilwarna.AmbilWarnaDialog.OnAmbilWarnaListener;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
