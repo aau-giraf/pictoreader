36686d4e952d63f0fba7cb920780b7395f4766d4
Lasse Nielsen <leni12@student.aau.dk>
2015-05-19 11:21:09 +0200

    Yow!  We're going to a new disco!

--- src/main/java/dk/aau/cs/giraf/pictoreader/SpeechBoardFragment.java
+++ src/main/java/dk/aau/cs/giraf/pictoreader/SpeechBoardFragment.java
@@ -512,8 +512,8 @@ public class SpeechBoardFragment extends Fragment implements ShowcaseManager.Sho
             @Override
             public void configShowCaseView(final ShowcaseView showcaseView) {
                 showcaseView.setShowcase(trashButtonTarget, true);
-                showcaseView.setContentTitle("Fjern pictogrammer");
-                showcaseView.setContentText("Her fjerner du pictogrammer der er tilføjet til sætnings kassen");
+                showcaseView.setContentTitle("Fjern piktogrammer");
+                showcaseView.setContentText("Her fjerner du piktogrammer der er tilføjet til sætnings kassen");
                 showcaseView.setStyle(R.style.GirafCustomShowcaseTheme);
                 showcaseView.setButtonPosition(centerRightButtonParams);
                 showcaseView.setTextPostion(textX, textY);
@@ -526,7 +526,7 @@ public class SpeechBoardFragment extends Fragment implements ShowcaseManager.Sho
             public void configShowCaseView(final ShowcaseView showcaseView) {
                 showcaseView.setShowcase(playButtonTarget, true);
                 showcaseView.setContentTitle("Afspil");
-                showcaseView.setContentText("Her afspiller du pictogrammerne der er tilføjet til sætnings kassen");
+                showcaseView.setContentText("Her afspiller du piktogrammerne der er tilføjet til sætnings kassen");
                 showcaseView.setStyle(R.style.GirafCustomShowcaseTheme);
                 showcaseView.setButtonPosition(centerRightButtonParams);
                 showcaseView.setTextPostion(textX, textY);
